c95cf7b28a59a179f67a54c8c3a12be34396be0f
youssefessmat <109749787+youssefessmat@users.noreply.github.com>
2022-11-28 19:46:23 +0200

    Update InternetPaymentFactory.java

--- src/Admin/InternetPaymentFactory.java
+++ src/Admin/InternetPaymentFactory.java
@@ -5,8 +5,6 @@ public class InternetPaymentFactory implements IServiceFactory {
 	@Override
 	public IService createService() {
 		// TODO Auto-generated method stub
-		System.out.println("heyy from essmat");
 		return null;
 	}
-
 }
