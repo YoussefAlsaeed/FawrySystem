a8a3ddf85620c701cef449150f4c8ff7f2a194f2
FatmaAhmed25 <fatmaahmed6555@gmail.com>
2022-11-28 19:38:50 +0200

    Update InternetPaymentFactory.java
    Co-Authored-By: Youssef Alsaeed <105588503+YoussefAlsaeed@users.noreply.github.com>

--- src/Admin/InternetPaymentFactory.java
+++ src/Admin/InternetPaymentFactory.java
@@ -5,6 +5,7 @@ public class InternetPaymentFactory implements IServiceFactory {
 	@Override
 	public IService createService() {
 		// TODO Auto-generated method stub
+		System.out.println("hii");
 		return null;
 	}
 
