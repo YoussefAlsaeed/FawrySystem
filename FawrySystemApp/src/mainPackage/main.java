53d05ede05657cd78d5fe5250b86ec1d5762f37d
youssefessmat <109749787+youssefessmat@users.noreply.github.com>
2022-11-29 22:35:32 +0200

    modified in main
    i modified the main in which i added a menu for registration and menue for the admin to add a form

--- FawrySystemApp/src/mainPackage/main.java
+++ FawrySystemApp/src/mainPackage/main.java
@@ -1,61 +1,130 @@
 package mainPackage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		FormBuilder builder=new FormBuilder();
 		String choice = "";
 		Scanner scan = new Scanner(System.in);
-		while (!choice.equals("3")) {
+        String username;
+        String password;
+        String email;
+        UserController c = new UserController();
+		while (!choice.equals("4"))
+		{
 			System.out.println("* * * * * * * * * * * * * * * * * * ");
 
-			System.out.println("Creation Menu: ");
+			System.out.println("registeration Menu: ");
 			System.out.println();
 			// System.out.println("1-Generate randoms slots");
-			System.out.println("1-Add drop down field");
-			System.out.println("2-Add Text Field");
-			System.out.println("3-Create Form");
+			System.out.println("1-login as users");
+			System.out.println("2-SignUp as users");
+			System.out.println("3-login as admin");
 			System.out.println();
 			System.out.println("* * * * * * * * * * * * * * * * * * ");
-
+			
 			System.out.println("Enter your choice: ");
 			choice = scan.next();// taking the user's choice
-
-			switch (choice) {
 			
-			case "1": 
-				System.out.println("Enter the name of the drop down field: ");
-				String name=scan.next();
-				System.out.println("Enter the number of the fields: ");
-				int no=scan.nextInt();
-				ArrayList<Object> array=new ArrayList<Object>();
-				for(int i=0;i<no;i++)
-				{
-					System.out.println("Enter value "+(i+1)+" : ");
-					array.add(scan.next());
-				}
-				builder.buildDropDownField(name, no, array);
-				break;
 			
-			case "2": 
-				System.out.println("Enter the name of the drop down field: ");
-				name=scan.next();
-				builder.buildTextField(name);
-				break;
-			case "3":
-				System.out.println("Please enter the name of the form: ");
-				name=scan.next();
-				builder.addName(name);
-				Form f=builder.Build();
-				f.print();
+			switch(choice)
+			{
+			
+			case"1":
+				System.out.println("What is your username: ");
+				 username=scan.next();
+				 
+			     System.out.println("What is your password: ");
+			     password= scan.next();
+				User user2=new User();
+				user2.setPassword(password);
+				user2.setUsername(username);
+				if(c.login(user2))
+					 System.out.println("Login Successful");
+				else
+					 System.out.println("there is no user with info please sign up first");
 				break;
 				
-			
-	}
+			case"2":
+				   System.out.println("What is your username: ");
+				  username=scan.next();
+				  
+			     System.out.println("What is your password: ");
+			     password= scan.next();
+			     
+			     System.out.println("What is your email: ");
+			     email= scan.next();
+			     User user=new User();
+			     user.setUsername(username);
+			     user.setPassword(password);
+			     user.setEmail(email);
+			      c.signUp(user);
+				break;
+				
+			case"3":
+				choice= "";
+				while (!choice.equals("3")) {
+					System.out.println("* * * * * * * * * * * * * * * * * * ");
+
+					System.out.println("Creation Menu: ");
+					System.out.println();
+					// System.out.println("1-Generate randoms slots");
+					System.out.println("1-Add drop down field");
+					System.out.println("2-Add Text Field");
+					System.out.println("3-Create Form");
+					System.out.println();
+					System.out.println("* * * * * * * * * * * * * * * * * * ");
+
+					System.out.println("Enter your choice: ");
+					choice = scan.next();// taking the user's choice
+
+					switch (choice)
+					{
+					
+					case "1": 
+						System.out.println("Enter the name of the drop down field: ");
+						String name=scan.next();
+						System.out.println("Enter the number of the fields: ");
+						int no=scan.nextInt();
+						ArrayList<Object> array=new ArrayList<Object>();
+						for(int i=0;i<no;i++)
+						{
+							System.out.println("Enter value "+(i+1)+" : ");
+							array.add(scan.next());
+						}
+						builder.buildDropDownField(name, no, array);
+						break;
+					
+					case "2": 
+						System.out.println("Enter the name of the drop down field: ");
+						name=scan.next();
+						builder.buildTextField(name);
+						break;
+					case "3":
+						System.out.println("Please enter the name of the form: ");
+						name=scan.next();
+						builder.addName(name);
+						Form f=builder.Build();
+						f.print();
+						break;
+						
+					
+			}
+				}
+				
+			}
+
 		}
+		
+		
+		
+
 
 }
 	}
